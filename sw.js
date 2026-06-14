59a36edd6bda672832c20a9579fdbcb5b28c96d7
anashath <alinashath@gmail.com>
2026-06-14 19:50:55 +0500

    SW cache v6

--- sw.js
+++ sw.js
@@ -1,5 +1,1 @@
-const CACHE = 'larobe-v5';
-const ASSETS = ['/larobe-orders/','/larobe-orders/index.html'];
-self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{})); self.skipWaiting(); });
-self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim(); });
-self.addEventListener('fetch',e=>{ e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))); });
+const CACHE="larobe-v6",ASSETS=["/larobe-orders/","/larobe-orders/index.html"];self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));self.skipWaiting()});self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});self.addEventListener("fetch",e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
