1ff84289fe4272ac3bd28db7682e4ea44f9eab25
anashath <alinashath@gmail.com>
2026-06-14 19:13:31 +0500

    Bump SW cache v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'larobe-v3';
+const CACHE = 'larobe-v4';
 const ASSETS = ['/larobe-orders/','/larobe-orders/index.html'];
 self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{})); self.skipWaiting(); });
 self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim(); });
